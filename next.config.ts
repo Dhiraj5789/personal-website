e4e8381d460ddb10ea6e25f0aca4fc74c8efa42e
Dhiraj Suthar <dhirajsuthar@Dhirajs-MacBook-Air.local>
2026-02-07 12:08:22 +0530

    static version

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+
+const nextConfig:NextConfig = {
+  output: "export",                       // generates static files
+  basePath: "/personal-website",            // your GitHub repo slug
+  assetPrefix: "/personal-website/",         // ensures assets load correctly
+  images: {
+    unoptimized: true                     // disables dynamic image optimization
+  }
 };
 
+module.exports = nextConfig;
 export default nextConfig;
